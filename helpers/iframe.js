d14c5e8f27399b44d0c75bf56ef0735a23e5893a
Alan <adong@users.noreply.github.com>
2019-08-01 14:16:56 -0700

    feat: add absolute path check and skip API prefix if absolute path (#90)
    * feat: add absolute path check and skip API prefix if its absolute path
    * feat: add handle for same page anchor tag

--- helpers/iframe.js
+++ helpers/iframe.js
@@ -6,6 +6,11 @@ const apiUrl = config.get('ecosystem.api');
 const apiVersion = 'v4';
 
 const iframeScript = `
+    function isAbsolutePath(href) {
+        const absolutePath = new RegExp('^((http|https)://|#)');
+        return absolutePath.test(href);
+    };
+
     function replaceHref(apiHref='${apiUrl}') {
         const currentIframeHref = new URL(document.location.href);
         const urlOrigin = currentIframeHref.origin;
@@ -21,6 +26,9 @@ const iframeScript = `
         const anchors = document.getElementsByTagName('a');
         for (let anchor of anchors) {
           let originalHref = anchor.attributes.href.value;
+          if (isAbsolutePath(originalHref)) {
+              continue;
+          }
           const newUrl = apiUrlOrigin + '/' + apiVersion + '/' + urlFileDir + '/' + originalHref;
           anchor.href = newUrl.replace('ARTIFACTS', 'artifacts') + '?type=preview';
           anchor.addEventListener('click', function(e) {
