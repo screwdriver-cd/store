d05ed28df0605b6c0a81a553cb0d42bbf1cb50ad
Alan <adong@users.noreply.github.com>
2019-12-05 12:24:25 -0800

    fix: incorporated mime-types library (#96)

--- helpers/mime.js
+++ helpers/mime.js
@@ -1,38 +1,16 @@
 'use strict';
 
+const mime = require('mime-types');
+
 /**
  * getMimeFromFileExtension
  * @param  {String} fileExtension  File extension (e.g. css, txt, html)
  * @return {String} text/html
  */
 function getMimeFromFileExtension(fileExtension) {
-    let mime = '';
-
-    switch (fileExtension.toLowerCase()) {
-    case 'html':
-        mime = 'text/html';
-        break;
-    case 'css':
-        mime = 'text/css';
-        break;
-    case 'js':
-        mime = 'text/javascript';
-        break;
-    case 'png':
-        mime = 'image/png';
-        break;
-    case 'jpeg':
-        mime = 'image/jpeg';
-        break;
-    case 'jpg':
-        mime = 'image/jpeg';
-        break;
-    default:
-        break;
-    }
-
-    return mime;
+    return mime.lookup(fileExtension) || '';
 }
+
 const knownMimes = ['text/css', 'text/javascript', 'image/png', 'image/jpeg'];
 const displableMimes = ['text/html'];
 
